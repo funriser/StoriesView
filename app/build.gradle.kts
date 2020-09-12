216c45202c79a4e101b7929aff96d49cdf506928
funriser <kost.maksym@gmail.com>
2020-09-12 20:19:46 +0100

    module's build gradle scripts migration to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.funrisestudio.stories.Libs
+import com.funrisestudio.stories.Modules
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.2")
+
+    defaultConfig {
+        applicationId = "com.funrisestudio.stories"
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(Libs.kotlin)
+    implementation(project(Modules.stories))
+    implementation(Libs.glide)
+    kapt(Libs.glideCompiler)
+    implementation(Libs.AndroidX.core)
+    implementation(Libs.AndroidX.appCompat)
+    implementation(Libs.AndroidX.material)
+
+}
---
+++ buildSrc/src/main/java/com/funrisestudio/stories/dependencies.kt
@@ -0,0 +1,41 @@
+package com.funrisestudio.stories
+
+object Versions {
+
+    const val kotlinVersion = "1.4.10"
+
+    const val glideVersion = "4.11.0"
+
+    object Plugins {
+        const val androidGradle = "4.2.0-alpha10"
+        const val kotlinGradle = kotlinVersion
+    }
+    object AndroidX {
+        const val core = "1.3.1"
+        const val appCompat = "1.2.0"
+        const val material = "1.2.1"
+    }
+}
+
+object Plugins {
+    const val androidGradle = "com.android.tools.build:gradle:${Versions.Plugins.androidGradle}"
+    const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.Plugins.kotlinGradle}"
+}
+
+object Libs {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glideVersion}"
+    const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glideVersion}"
+
+    object AndroidX {
+        const val core = "androidx.core:core-ktx:${Versions.AndroidX.core}"
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.AndroidX.appCompat}"
+        const val material = "com.google.android.material:material:${Versions.AndroidX.material}"
+    }
+
+}
+
+object Modules {
+    const val stories = ":stories"
+}
